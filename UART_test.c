688bca4dbe6fbf587dbec9d7b7aefee02f271bb6
Varsha-K <varsha.k209@gmail.com>
2018-05-31 23:03:58 -0400

    Add files via upload

---
+++ UART_test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() 
+{ 
+	char byte_written,*byte_read,dummy;
+  	FILE * MEM;
+	size_t result;
+
+        /* Opening the memory driver */
+	MEM = fopen("/dev/memory", "rb");
+
+	/* No buffering for file i/o */
+  	setvbuf(MEM,&dummy,_IONBF,1);
+
+  	/* Initializing byte_written */
+  	printf("enter characters\n");
+        byte_written = scanf("%c",&dummy);
+	byte_read = (char*) malloc (sizeof(char));
+   
+  	/* Writing to the memory driver */
+  	printf("Byte value written is %c\n",byte_written);
+  	fwrite(&byte_written,1,1,MEM);
+	fclose (MEM);
+
+	/* Reading from memory driver */
+	MEM = fopen("/dev/memory", "rb");
+  	result = fread(byte_read,1,1,MEM);	
+  	printf("Byte value read is %c\n",*byte_read);
+	printf("Number of bytes read is %d\n",result);
+
+  	fclose(MEM);
+	
+}
